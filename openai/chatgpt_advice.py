8e98c42d00c495edbda54ed1f479212f07f9dd1d
XCV0 <frt.mrartem@gmail.com>
2023-10-20 19:18:58 +0300

    Update chatgpt_advice.py

--- openai/chatgpt_advice.py
+++ openai/chatgpt_advice.py
@@ -3,19 +3,26 @@ import asyncio
 
 client = httpx.AsyncClient()
 model = 'gpt-3.5-turbo'
-history = [{"role": "system", "content": "You are a helpful assistant."},
-           {"role": "user", "content": "вопрос"}]
+
 user = 123
 
-result = await client.post(
-    url = 'https://api.openai.com/v1/chat/completions',
-    headers={
-        "Authorization": "Bearer" + "",
-    },
-    json = {
-        'model': model,
-        'messages': history,
-        'user': user
-    },
-    timeout=20
-)
+
+async def get_advice(weather):
+    history = [{"role": "system", "content": f"Посоветуй что одеть по этой погоде\n"
+                                             f"{weather}"},
+               {"role": "user", "content": "вопрос"}]
+
+    result = await client.post(
+        url='https://api.openai.com/v1/chat/completions',
+        headers={
+            "Authorization": "Bearer" + "",
+        },
+        json={
+            'model': model,
+            'messages': history,
+            'user': user 
+        },
+        timeout=20
+    )
+
+    return result['choice'][0]['message']
